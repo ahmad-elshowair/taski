5acc4b37b57a2ce074978566364b92b62272fa6d
Ahmad Elshowair <it.is.work@outlook.com>
2022-07-04 14:00:51 +0200

    add a new todo to the list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import { InputField } from './components/InputField';
+import { Todo } from './models/todo';
+import { nanoid } from 'nanoid';
 
 const App: React.FC = ()=> {
+  const [todo, setTodo] = React.useState<string>("");
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
+  const add = (event: React.FormEvent)=>{
+    event.preventDefault();
+    if(todo){
+      setTodos(prevTodos => {
+        return [
+          ...prevTodos,
+          {
+            id: nanoid(),
+            todo,
+            isDone: false
+          }
+        ];
+      });
+    }
+  }
+  console.log(todos);
   return (
     <div className="app bg-info">
       <header className='header'>
         <h1 className="heading text-uppercase">Taski</h1>
       </header>
-      <InputField/>
+      <InputField 
+        todo={todo} 
+        setTodo={setTodo}
+        add={add}
+      />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-export  const InputField: React.FC = () =>{
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  add: (event: React.FormEvent) => void
+}
+
+export  const InputField: React.FC<Props> = ({todo, setTodo, add}) =>{
   return(
       <section className="input-section"> 
         <div className="container">
-          <div className="row mb-5">
+          <form className="row mb-5" onSubmit={add}>
             <input 
               type="text" 
               id='search' 
               name="search"
               className="form-control-lg col-md-12"
-              placeholder="type your task"
+              placeholder="type your task..."
+              value={todo}
+              onChange={(event) => setTodo(event.target.value)}
             />
             <button className="btn btn-primary text-uppercase">add</button>
-          </div>
+          </form>
         </div>
       </section>
   );
